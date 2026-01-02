f3aba2bdcbcd095f5d143a9956ad72105dd662b2
Mika Senghaas <mail@mikasenghaas.de>
2026-01-02 12:18:10 +0100

    move social icon assets

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
+import emailSvg from "@/app/assets/email.svg";
+import linkedinSvg from "@/app/assets/linkedin.svg";
+import pinterestSvg from "@/app/assets/pinterest.svg";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
@@ -11,7 +14,7 @@ export default function Footer() {
           href="mailto:caeciliebode@gmail.com"
           aria-label="Email"
         >
-          <Image src="/email.svg" alt="Email" width={32} height={32} />
+          <Image src={emailSvg} alt="Email" width={32} height={32} />
         </Link>
         <Link
           href="https://www.linkedin.com/in/c%C3%A6cilie-lid%C3%A9n-bode-8745a025a/"
@@ -19,7 +22,7 @@ export default function Footer() {
           rel="noopener noreferrer"
           aria-label="LinkedIn"
         >
-          <Image src="/linkedin.svg" alt="LinkedIn" width={32} height={32} />
+          <Image src={linkedinSvg} alt="LinkedIn" width={32} height={32} />
         </Link>
         <Link
           href="https://pin.it/7Jg9C1reP"
@@ -27,7 +30,7 @@ export default function Footer() {
           rel="noopener noreferrer"
           aria-label="Pinterest"
         >
-          <Image src="/pinterest.svg" alt="Pinterest" width={32} height={32} />
+          <Image src={pinterestSvg} alt="Pinterest" width={32} height={32} />
         </Link>
       </div>
       <p className="text-sm">
