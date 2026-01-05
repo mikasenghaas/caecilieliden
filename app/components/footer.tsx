aab6d6fc53291c0ad1d4a96fa917d1d2fca93ad5
Mika Senghaas <mail@mikasenghaas.de>
2026-01-05 21:38:40 +0100

    convert name tag + social icons from svg->png

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
-import emailSvg from "@/app/assets/email.svg";
-import linkedinSvg from "@/app/assets/linkedin.svg";
-import pinterestSvg from "@/app/assets/pinterest.svg";
+import emailPng from "@/app/assets/email.png";
+import linkedinPng from "@/app/assets/linkedin.png";
+import pinterestPng from "@/app/assets/pinterest.png";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
@@ -14,7 +14,7 @@ export default function Footer() {
           href="mailto:caeciliebode@gmail.com"
           aria-label="Email"
         >
-          <Image src={emailSvg} alt="Email" width={32} height={32} unoptimized />
+          <Image src={emailPng} alt="Email" width={32} height={32} />
         </Link>
         <Link
           href="https://www.linkedin.com/in/c%C3%A6cilie-lid%C3%A9n-bode-8745a025a/"
@@ -22,7 +22,7 @@ export default function Footer() {
           rel="noopener noreferrer"
           aria-label="LinkedIn"
         >
-          <Image src={linkedinSvg} alt="LinkedIn" width={32} height={32} unoptimized />
+          <Image src={linkedinPng} alt="LinkedIn" width={32} height={32} />
         </Link>
         <Link
           href="https://pin.it/7Jg9C1reP"
@@ -30,7 +30,7 @@ export default function Footer() {
           rel="noopener noreferrer"
           aria-label="Pinterest"
         >
-          <Image src={pinterestSvg} alt="Pinterest" width={32} height={32} unoptimized />
+          <Image src={pinterestPng} alt="Pinterest" width={32} height={32} />
         </Link>
       </div>
       <p className="text-sm">
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
-import caecilieSvg from "@/app/assets/caecilie.svg";
+import caeciliePng from "@/app/assets/caecilie.png";
 
 export default function Header() {
   return (
     <header className="w-full flex flex-col items-center pt-12 pb-8 sm:pt-16 sm:pb-8 md:pt-28 md:pb-14">
       <Image
-        src={caecilieSvg}
+        src={caeciliePng}
         alt="Caecilie Lidèn Bode"
         className="w-full sm:max-w-md md:max-w-lg"
         priority
-        unoptimized
       />
     </header>
   );
